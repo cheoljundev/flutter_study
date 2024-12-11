e22cae6db7e6cc92f13143f3ee933767e3a0f40b
cheoljundev <cheoljundev@gmail.com>
2024-12-11 21:23:35 +0900

    collection : Set

---
+++ lib/ch4/collection/set.dart
@@ -0,0 +1,13 @@
+main() {
+  Set<int> set1 = {10, 20, 10};
+  print(set1);
+  set1.add(30);
+  set1.add(40);
+  print(set1);
+
+  Set<int> set2 = Set();
+  set2.add(10);
+  set2.add(20);
+  print(set2);
+
+}
